919410b790dce20f671c8503733a675c7a408419
suffermore <cjclancelot@gmail.com>
2015-01-19 15:05:16 -0500

    commit maxSubArray

--- LeetCodeSolutions/src/highfre/MaxSubArray.java
+++ LeetCodeSolutions/src/highfre/MaxSubArray.java
@@ -7,14 +7,23 @@ public class MaxSubArray {
         int maxSum = Integer.MIN_VALUE;
         
         for (int i = 0; i < A.length; i++) {
-
             maxSum = Math.max(maxSum, curSum + A[i]);
             curSum = Math.max(0, curSum + A[i]);
-            
         }
         return maxSum;
     }
-	
+    
+    public int maxSubArrayII(int[] A) {
+    	
+        int curSum = A[0];
+        int maxSum = A[0];
+        
+        for (int i = 1; i < A.length; i++) {
+            curSum = Math.max(A[i], curSum + A[i]); //cur_sum < 0 ? A[i] : cur_sum + A[i]
+            maxSum = Math.max(maxSum, curSum);
+        }
+        return maxSum;
+    }
 	public static void main(String[] args) {
 		int[] A = {-1, 2};
 		
--- LeetCodeSolutions/src/highfre/MaxSubArrayII.java
+++ LeetCodeSolutions/src/highfre/MaxSubArrayII.java
@@ -2,8 +2,40 @@ package highfre;
 
 public class MaxSubArrayII {
 	
-	//O(n^2)
+	//O(n)
 	public int maxSubArray(int[] A) {
+		if (A == null || A.length < 2) {
+			return 0;
+		}
+		
+		//left
+		int[] left = new int[A.length];
+		left[0] = A[0];
+		int cur_sum = Integer.MIN_VALUE;
+		for (int i = 1; i < A.length; i++) {
+			cur_sum = Math.max(A[i], cur_sum + A[i]);	//cur_sum < 0 ? A[i] : cur_sum + A[i]
+			left[i] = Math.max(left[i-1], cur_sum);
+		}
+		
+		//right
+		int[] right = new int[A.length];
+		right[A.length - 1] = A[A.length - 1];
+		cur_sum = Integer.MIN_VALUE;
+		for (int i = A.length - 2; i >= 0; i--) {
+			cur_sum = Math.max(A[i], cur_sum + A[i]);
+			right[i] = Math.max(right[i + 1], cur_sum);
+		}
+		
+		//get max
+		int profit = Integer.MIN_VALUE;
+		for (int i = 0; i < A.length - 1; i++) {
+			profit = Math.max(profit, left[i] + right[i + 1]);
+		}
+		return profit;
+	}
+	
+	//O(n^2)
+	public int maxSubArrayII(int[] A) {
 		if (A == null || A.length < 2) {
 			return 0;
 		}
@@ -16,11 +48,11 @@ public class MaxSubArrayII {
 	}
 	
 	private int divMaxSubArray(int[] A, int start, int end) {
-		int cur_max = 0;
+		int cur_sum = 0;
 		int max = Integer.MIN_VALUE;
 		for (int i = start; i <= end; i++) {
-			max = Math.max(max, cur_max + A[i]);
-			cur_max = Math.max(cur_max + A[i], 0);
+			max = Math.max(max, cur_sum + A[i]);
+			cur_sum = Math.max(cur_sum + A[i], 0);
 		}
 		return max;
 	}
