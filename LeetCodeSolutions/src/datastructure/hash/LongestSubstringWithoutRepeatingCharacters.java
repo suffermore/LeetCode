4d580209a11f191db01553d4ee470ffbf2bace84
suffermore <cjclancelot@gmail.com>
2015-01-23 01:31:21 -0500

    commit longestSubstringwithoutrepeatingcharacters

---
+++ LeetCodeSolutions/src/datastructure/hash/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,35 @@
+package datastructure.hash;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+	public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        HashSet<Character> set = new HashSet<Character>();
+        
+        int leftBound = 0, max = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                while (leftBound < i && s.charAt(leftBound) != s.charAt(i)) {
+                    set.remove(s.charAt(leftBound));
+                    leftBound ++;
+                }
+                leftBound ++;
+            } else {
+                set.add(s.charAt(i));
+                max = Math.max(max, i - leftBound + 1);
+            }
+        }
+        
+        return max;
+    }
+	
+	public static void main(String[] args) {
+		LongestSubstringWithoutRepeatingCharacters l = new LongestSubstringWithoutRepeatingCharacters();
+		System.out.println(l.lengthOfLongestSubstring("abva"));
+	}
+}
