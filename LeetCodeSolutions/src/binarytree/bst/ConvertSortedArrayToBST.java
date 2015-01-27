1bb8d8d833ac8051e6f7abb13a7b15e3dffbc1f7
suffermore <cjclancelot@gmail.com>
2015-01-27 12:06:53 -0500

    commit convertSortedArrayToBST

---
+++ LeetCodeSolutions/src/binarytree/bst/ConvertSortedArrayToBST.java
@@ -0,0 +1,23 @@
+package binarytree.bst;
+
+import binarytree.TreeNode;
+
+public class ConvertSortedArrayToBST {
+
+	public TreeNode sortedArrayToBST(int[] num) {
+        if (num == null) {
+            return null;
+        }
+        return buildBST(num, 0, num.length - 1);
+    }
+    private TreeNode buildBST(int[] num, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(num[mid]);
+        root.left = buildBST(num, start, mid - 1);
+        root.right = buildBST(num, mid + 1, end);
+        return root;
+    }
+}
