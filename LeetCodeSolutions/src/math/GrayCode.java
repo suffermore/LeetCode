bdf18e26eb7cad05fb7cfb46ff83a2545273ee06
suffermore <cjclancelot@gmail.com>
2015-03-15 15:53:27 -0400

    commit graycode

--- LeetCodeSolutions/src/math/GrayCode.java
+++ LeetCodeSolutions/src/math/GrayCode.java
@@ -26,6 +26,24 @@ public class GrayCode {
 		return result;
     }
 	
+	public List<Integer> grayCodeII(int n) {
+        List<Integer> result = new ArrayList<Integer>();
+		result.add(0);
+		
+		if (n == 0) {
+			return result;
+		}
+
+        for (int i = 1; i <= n; i++) {
+		    int addNumber = 1 << (i - 1);
+            for (int j = result.size() - 1; j >= 0; j--) {
+                result.add(result.get(j) + addNumber);
+            }
+        }
+        
+        return result;
+    }
+
 	public static void main(String[] args) {
 		GrayCode g = new GrayCode();
 		g.grayCode(1);
