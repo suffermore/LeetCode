450d3fb748813f83156ad5f784d05065f5900e50
suffermore <cjclancelot@gmail.com>
2015-04-04 00:13:47 -0400

    reverse bits

---
+++ LeetCodeSolutions/src/datastructure/hash/ReverseBits.java
@@ -0,0 +1,20 @@
+package datastructure.hash;
+
+public class ReverseBits {
+	// you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int rst = 0;
+        int k = 32;
+        while (k > 0) {
+            rst = (rst << 1) + (n & 1);
+            n = n >> 1;
+            k--;
+        }
+        return rst;
+    }
+    
+    public static void main(String[] args) {
+		ReverseBits r = new ReverseBits();
+		System.out.println(r.reverseBits(2));
+	}
+}
