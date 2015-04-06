832a66353831ed0b5f0a6fb2234f49d1c6abe610
suffermore <cjclancelot@gmail.com>
2015-04-05 23:32:11 -0700

    commit implementIteratorBST

--- LeetCodeSolutions/src/binarytree/bst/ImplementIteratorOfBST.java
+++ LeetCodeSolutions/src/binarytree/bst/ImplementIteratorOfBST.java
@@ -14,34 +14,66 @@ import binarytree.TreeNode;
  */
 
 public class ImplementIteratorOfBST {
-	private Stack<TreeNode> stack;
-	
+//	private Stack<TreeNode> stack;
+//	
+//    public ImplementIteratorOfBST(TreeNode root) {
+//        stack = new Stack<TreeNode>();
+//        TreeNode tempNode = root;
+//        if(root!=null) {
+//            while(tempNode != null){
+//                stack.push(tempNode);
+//                tempNode = tempNode.left;
+//            }
+//        }
+//    }
+//
+//    /** @return whether we have a next smallest number */
+//    public boolean hasNext() {
+//        return !stack.empty();
+//    }
+//
+//    /** @return the next smallest number */
+//    public int next() {
+//        TreeNode node = stack.pop();
+//        TreeNode tempNode = node.right;
+//        if(tempNode!=null){
+//            while(tempNode!=null){
+//                stack.push(tempNode);
+//                tempNode = tempNode.left;
+//            }
+//        }
+//        return node.val;
+//    }
+
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode cur = new TreeNode(0);
     public ImplementIteratorOfBST(TreeNode root) {
-        stack = new Stack<TreeNode>();
-        TreeNode tempNode = root;
-        if(root!=null) {
-            while(tempNode != null){
-                stack.push(tempNode);
-                tempNode = tempNode.left;
-            }
-        }
+        cur = root;
     }
 
     /** @return whether we have a next smallest number */
     public boolean hasNext() {
-        return !stack.empty();
+        return ((cur != null) || !stack.isEmpty());
     }
 
     /** @return the next smallest number */
     public int next() {
-        TreeNode node = stack.pop();
-        TreeNode tempNode = node.right;
-        if(tempNode!=null){
-            while(tempNode!=null){
-                stack.push(tempNode);
-                tempNode = tempNode.left;
-            }
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
         }
-        return node.val;
+        cur = stack.pop();
+        TreeNode tar = cur;
+        cur = cur.right;
+        return tar.val;
     }
+    
+    public static void main(String[] args) {
+    	TreeNode root = new TreeNode(2);
+//    	root.left = new TreeNode(1);
+		ImplementIteratorOfBST i = new ImplementIteratorOfBST(root);
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
+	}
 }
